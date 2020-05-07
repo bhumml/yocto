066b3b021b744d19b3f2af04188b347dee16e4cb
Benedikt Humml <ben.humml@gmail.com>
2020-05-08 00:54:59 +0200

    spam

--- build_docker.sh
+++ build_docker.sh
@@ -3,6 +3,8 @@
 # # Pull Repos
 git pull --recurse-submodules
 # Source Environment
+echo 'Das ist ein Beispiel!' 
+
 source poky-jethro/oe-init-build-env bbb;
 # Bitbake
 bitbake core-image-minimal;
