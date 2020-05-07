551c43f38d86fbf8ddc354414cd27b30ab7de945
Benedikt Humml <ben.humml@gmail.com>
2020-05-07 04:30:26 +0200

    fixes

--- build_docker.sh
+++ build_docker.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-source /home/$USER/yocto/poky-jethro/oe-init-build-env /home/$USER/yocto/bbb;
-bitbake core-image-weston;
+source /home/$USER/yocto/poky-jethro/oe-init-build-env /home/$USER/yocto/bbb/;
+pwd;
+bitbake core-image-minimal;
+pwd;
