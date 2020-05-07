35e1b6b68586f5b9be797f663cda5a48db6452f9
Benedikt Humml <ben.humml@gmail.com>
2020-05-08 00:56:11 +0200

    assa

--- build_docker.sh
+++ build_docker.sh
@@ -5,6 +5,6 @@ git pull --recurse-submodules
 # Source Environment
 echo 'Das ist ein Beispiel!' 
 
-source poky-jethro/oe-init-build-env bbb;
+source /home/$USER/yocto/poky-jethro/oe-init-build-env /home/$USER/yocto/bbb;
 # Bitbake
 bitbake core-image-minimal;
